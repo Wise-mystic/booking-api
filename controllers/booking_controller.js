96a65a2c609c9f5d163d93b8a065261623fa00d4
Wisdom Kwawuvi <wisdomkwawuvi23@gmail.com>
2024-10-03 17:16:12 +0000

    gh

--- controllers/booking_controller.js
+++ controllers/booking_controller.js
@@ -3,8 +3,8 @@ import { Booking } from "../model/booking-model.js";
 
 export const getAllBookings = async (req, res, next) => {
     try {
-        await Booking.find(req.body);
-        res.status(200).json('These are all bookings');
+       const bookings = await Booking.find();
+        res.status(200).json(bookings);
     } catch (error) {
         next(error)
     }
@@ -19,7 +19,9 @@ export const postBooking = async (req, res, next) => {
         const newBooking = new Booking(req.body);
         const bookings = await newBooking.save();
 
-        res.status(201).json(bookings);
+        // await Booking.create(req.body);
+
+        res.status(201).json("Booking was added!");
 
     } catch (error) {
         next(error);
@@ -29,7 +31,7 @@ export const postBooking = async (req, res, next) => {
 export const updateBooking = async (req, res, next) => {
 
 
-    res.status(200).json(Booking);
+    res.status(200).json(booking);
 
 }
 
